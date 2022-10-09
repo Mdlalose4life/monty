16075560b23cb833573e6be68b002d430ad31e04
Mdlalose4life <blessing1059@gmail.com>
2022-10-10 00:22:02 +0200

    project push

---
+++ pstr.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+/**
+ * f_pchar - prints the char at the top of the stack,
+ * followed by a new line
+ * @head: double pointer to stack's head
+ * @counter: counts the lines executed.
+ * Return: void
+*/
+void f_pchar(stack_t **head, unsigned int counter)
+{
+	stack_t *temp;
+
+	temp = *head;
+	if (!temp)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
+		fclose(var.file);
+		free(var.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	if (temp->n > 127 || temp->n < 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
+		fclose(var.file);
+		free(var.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", temp->n);
+
