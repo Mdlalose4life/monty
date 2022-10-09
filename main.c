dd646e1e0309ef25a4a8033cd0463a693b465272
Mdlalose4life <blessing1059@gmail.com>
2022-10-10 01:13:51 +0200

    Betty complient

--- main.c
+++ main.c
@@ -1,12 +1,4 @@
 #include "monty.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-#include <ctype.h>
 bus_t var = {NULL, NULL, NULL, 0};
 
 /**
